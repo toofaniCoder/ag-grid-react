a1c31cd0194cf15fc5ba956868fbcac4590fef0e
subroto11111 <toofanicoder333@gmail.com>
2025-05-14 13:13:13 +0530

    starter inital code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { AgGridReact } from 'ag-grid-react';
+import { useState, useEffect } from 'react'
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-yellow-100 min-h-screen">
+        <div className="container mx-auto">
+          <h1 className='text-blue-500 font-bold text-7xl'>AG Grid</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
